5bcf4d3100203a8c7137ddf03750d0b672c0227c
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-04-15 12:15:03 +0700

    update register page

---
+++ pages/register.php
@@ -0,0 +1,80 @@
+<?php 
+require __DIR__ . '/components/header.php'; 
+?>
+
+
+<?php
+    // Define database connection constants
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'cv_web');
+
+    // Connect to the database
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    // Check if the form is submitted
+    if (isset($_POST['login_btn'])) {
+        // Get the form data
+        $fullname = mysqli_real_escape_string($conn, $_POST['fullname']);
+        $username = mysqli_real_escape_string($conn, $_POST['name']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+
+        // Validate inputs
+
+        if ($password != $confirm_password) {
+            $error_msg = 'Passwords do not match';
+        } else {
+            // Hash the password
+            // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Insert user data into the database
+            $query = "INSERT INTO users (fullname, username, password) VALUES ('$fullname', '$username', '$password')";
+            $result = mysqli_query($conn, $query);
+
+            // Check if the insert was successful
+            if ($result) {
+                $success_msg = 'Registration successful';
+            } else {
+                $error_msg = 'Registration failed';
+            }
+        }
+    }
+?>
+<section>
+    <div class="register">
+        <div class="container">
+            <h1>REGISTER</h1>
+            <?php if (isset($error_msg)): ?>
+                <div class="error"><?php echo $error_msg; ?></div>
+            <?php endif; ?>
+            <?php if (isset($success_msg)): ?>
+                <div class="success"><?php echo $success_msg; ?></div>
+            <?php endif; ?>
+            <div class="login_form">
+                <form method="post">
+                    <label for="name">Fullname</label><br>
+                    <input type="text" id="name" name="fullname" placeholder="Type your full name" pattern="^[a-zA-ZàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹĐđ\s]+$" title="Fullname must start with a letter and be at least 8 characters long" required><br>
+                    <label for="name">Username</label><br>
+                    <input type="text" id="name" name="name" placeholder="Type your username" pattern="^[a-zA-Z][a-zA-Z0-9]{7,}$" title="Username must start with a letter and be at least 8 characters long" required><br>
+                    <label for="password">Password</label><br>
+                    <input type="password" id="password" name="password" placeholder="Type your password" minlength="8" required><br>
+                    <label for="password">Confirm Password</label><br>
+                    <input type="password" id="confirm_password" name="confirm_password" placeholder="Type your password" minlength="8" required><br></br>
+                    <button type="submit" name="login_btn">SIGN UP</button>
+                </form> 
+            </div>
+            <h4>Already have an account<a href="/login"> Sign in</a></h4>
+        </div>
+        <div class="image">
+            <img src="../images/CV.jpg" alt="">
+        </div>
+    </div>
+</section>
+
+
+
+
+</body>
+</html>
