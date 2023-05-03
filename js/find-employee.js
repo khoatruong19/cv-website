ec1fcc95299fb3785b5e1a3dfe2123241c8ba274
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-05-03 18:36:11 +0700

    add field level to cvs

--- js/find-employee.js
+++ js/find-employee.js
@@ -12,9 +12,25 @@ const locationInputEle = document.querySelector("#location-input")
 
 
 const handleSubmitFilter = () => {
-    console.log(positionInputEle.value)
-    console.log(levelInputEle.value)
-    console.log(experienceInputEle.value)
-    console.log(JSON.parse(skillsInputEle.value).map(item => item.value).join(','))
-    console.log(locationInputEle.value)
+    const formEle = document.getElementsByClassName("form-data")
+    let formData = new FormData()
+    for(let count = 0; count < formEle.length; count++){
+        if(formEle[count].name){
+            formData.append(formEle[count].name, formEle[count].value)
+        } 
+    }
+    document.getElementById('submit').disabled = true;
+    const ajax_request = new XMLHttpRequest();
+
+    ajax_request.open("POST", "find-employee", true)
+
+    ajax_request.send(formData)  
+
+    ajax_request.onreadystatechange = function (){
+        if(ajax_request.readyState == 4 && ajax_request.status == 200){
+            document.getElementById('submit').disabled = false;
+            console.log("Send")
+        }
+    }
+    return false;
 }
