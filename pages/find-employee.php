5d20ef1780306a8db33cb42148338ba9275a1666
HuyHieu-19 <104619096+HuyHieu-19@users.noreply.github.com>
2023-05-08 16:53:48 +0700

    .

--- pages/find-employee.php
+++ pages/find-employee.php
@@ -15,10 +15,10 @@
       rel="stylesheet"
       type="text/css"
     />
-  <script src="https://kit.fontawesome.com/7633094b57.js" crossorigin="anonymous"></script> 
+  <script src="https://kit.fontawesome.com/7633094b57.js" crossorigin="anonymous"></script>
 
-   <script src="https://cdn.tailwindcss.com"></script> 
- 
+   <script src="https://cdn.tailwindcss.com"></script>
+
   <script>
     tailwind.config = {
       theme: {
@@ -29,17 +29,6 @@
         }
       }
     }
-<<<<<<< HEAD
-=======
-    function clearAllFilter(){
-    const formEle = document.getElementsByClassName("form-data") 
-    for(let count = 0; count < formEle.length; count++){
-       if(formEle[count].name === "position" || formEle[count].name === "level" || 
-       formEle[count].name === "experience") formEle[count].value = "all"
-       else formEle[count].value = ""
-    }
-}
->>>>>>> main
   </script>
 
 </head>
@@ -50,7 +39,7 @@
 
     <div class="flex items-start h-52 p-5 bg-primary">
         <div class="w-[50%]">
-            <?php require __DIR__ . '/components/row-logo.php';  ?> 
+            <?php require __DIR__ . '/components/row-logo.php';  ?>
         </div>
         <h1 class="text-4xl font-semibold mt-8 ml-[-62px] text-center">FIND THE PERFECT FIT FOR YOUR TEAM</h1>
     </div>
@@ -59,13 +48,9 @@
         <form id="filter-form" onsubmit="handleSubmitFilter();return false;" action="#" method="post" class="flex flex-col gap-7 w-[40%] h-[80vh] shadow-2xl bg-white translate-y-[-10%] translate-x-[5%] rounded-lg overflow-y-auto py-3 px-5">
             <div class="flex items-center justify-between">
                 <h2 class="text-3xl font-semibold">Filter</h2>
-<<<<<<< HEAD
                 <span class="text-xl text-red-500 font-bold hover:opacity-70 cursor-pointer">Clear all</span>
-=======
-                <span onclick="clearAllFilter()" class="text-xl text-red-500 font-bold hover:opacity-70 cursor-pointer">Clear all</span>
->>>>>>> main
             </div>
-            <?php require __DIR__ . '/components/divider.php';  ?> 
+            <?php require __DIR__ . '/components/divider.php';  ?>
             <div class="flex items-center gap-5">
                 <h3 class="text-2xl font-semibold">Position</h3>
                 <select name="position" id="position-input" class="form-data outline-none w-full p-2 border-2 border-primary rounded-md appearance-none cursor-pointer">
@@ -75,7 +60,7 @@
                     <option value="Full-stack Developer">Full-stack Developer</option>
                 </select>
             </div>
-            <?php require __DIR__ . '/components/divider.php';  ?> 
+            <?php require __DIR__ . '/components/divider.php';  ?>
             <div class="flex items-center gap-5">
                 <h3 class="text-2xl font-semibold">Level</h3>
                 <select name="level" id="level-input" class="form-data outline-none w-full p-2 border-2 border-primary rounded-md appearance-none cursor-pointer">
@@ -88,7 +73,7 @@
                     <option value="Leader">Leader</option>
                 </select>
             </div>
-            <?php require __DIR__ . '/components/divider.php';  ?> 
+            <?php require __DIR__ . '/components/divider.php';  ?>
             <div class="flex items-center gap-5">
                 <h3 class="text-2xl font-semibold">Years Of Experience</h3>
                 <select name="experience" id="experience-input" class="form-data outline-none w-full p-2 border-2 border-primary rounded-md appearance-none cursor-pointer max-w-[185px]">
@@ -100,7 +85,7 @@
                     <option value="5-100">More than 5 years</option>
                 </select>
             </div>
-            <?php require __DIR__ . '/components/divider.php';  ?> 
+            <?php require __DIR__ . '/components/divider.php';  ?>
             <div class="flex items-center gap-5">
                 <h3 class="text-2xl font-semibold">Skills</h3>
                 <input
@@ -111,7 +96,7 @@
                     autofocus
                 />
             </div>
-            <?php require __DIR__ . '/components/divider.php';  ?> 
+            <?php require __DIR__ . '/components/divider.php';  ?>
             <div class="flex items-center gap-5">
                 <h3 class="text-2xl font-semibold bg-success">Location</h3>
                 <input
@@ -127,7 +112,7 @@
         <div class="w-[60%]">
             <form action="#" onsubmit="handleSubmitFilter(1, true);return false;" method="post">
                 <div class="w-[500px] mx-auto flex items-center gap-2 h-12 px-4 bg-white rounded-2xl shadow-2xl translate-y-[-97%]">
-                    <input id="job-title-input" class="flex-1 h-[100%] outline-none" type="text" placeholder="Search for job title" />   
+                    <input id="job-title-input" class="flex-1 h-[100%] outline-none" type="text" placeholder="Search for job title" />
                     <button type="submit">
                      <i class="fa-solid fa-magnifying-glass"></i>
                     </button>
@@ -138,15 +123,15 @@
                 <div id="cvs-list" class="mt-0 flex flex-col gap-6 h-[100%] h-full pr-6">
 
 
-           
+
                 </div>
                 <div id="pagination" class="flex items-center gap-2 max-w-[200px] mx-auto">
                 </div>
-               
+
             </div>
-            
+
         </div>
-       
+
 
     </div>
 
