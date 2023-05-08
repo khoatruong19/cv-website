4ecfba20942aee234f8d9263ec5578576a4f46a4
nqhuy137 <nquochuy137@gmail.com>
2023-05-08 20:41:43 +0700

    finished view-employee

--- pages/view-employee.php
+++ pages/view-employee.php
@@ -7,10 +7,9 @@
 <body>
 
 <?php 
-<<<<<<< HEAD
     include "dbcontroller.php";
     $id = $_GET['id'];
-    $sql = "SELECT * FROM cvs WHERE id = 1";
+    $sql = "SELECT * FROM cvs WHERE id = $id";
     $result = mysqli_query($conn, $sql);
     // echo $result;
     $row = mysqli_fetch_assoc($result);
@@ -27,76 +26,56 @@
     // $id = $_GET["id"];
     // echo $id;
 ?>
+<div id="dom_skills" style="display: none">
+    <?php 
+        echo htmlspecialchars($skills);
+    ?>
+</div>
+<script>
+    let dom_skills = document.getElementById("dom_skills");
+    let skills = dom_skills.textContent;
+    let skills_array = skills.split(";");
+    let skills_string = skills_array.join(", ");
+</script>
 
 <div class="container" style="width: 70vw">
-=======
-    $sql = 'SELECT '
-?>
-
-<div class="container">
->>>>>>> main
     <div class="row short-des">
         <div class="col-sm-4 user-image">
             <img src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" class="w-100 m-0">
         </div>
         <div class="col-sm-8 pl-6">
             <h1 class="username row">
-<<<<<<< HEAD
-            <div class="col fs-1 fw-bold mb-3"><span><?php echo "$first_name"?></span> <span><?php $array = array(1 => 'foo', 2 => 'bar'); echo "$last_name"; echo `<script>alert($array);</script>`;?></span></div>
+            <div class="col fs-1 fw-bold mb-3"><span><?php echo "$first_name"?></span><span><?php echo " $last_name"?></span></div>
             </h1>
             <div class="row mb-3">
                 <div class="col">
                     <span class="fw-bold h5">Short description</span><br>
                     <span class="fw-light p"><?php echo "$bio"?></span>
-=======
-                <div class="col fs-1 fw-bold mb-3">John Doe</div>
+                </div>
             </h1>
             <div class="row mb-3">
                 <div class="col">
-                    <span class="fw-bold">Short description</span><br>
-                    <span class="fw-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex dolorem alias impedit maxime odio iusto dolore aut facilis, quod assumenda debitis, unde quo! Similique, tempora temporibus. Maiores labore aperiam odio!</span>
->>>>>>> main
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col">
-<<<<<<< HEAD
                     <span class="fw-bold h5">Currently</span><br>
-                    <span class="fw-light p">Front-end developer</span>
-=======
-                    <span class="fw-bold">Currently</span><br>
-                    <span class="fw-light">Front-end developer</span>
->>>>>>> main
+                    <span class="fw-light p"><?php echo "$job_title"?></span>
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col">
-<<<<<<< HEAD
                     <span class="fw-bold h5">Skills</span><br>
-                    <span class="fw-light p">ReactJS</span>
-=======
-                    <span class="fw-bold">Skills</span><br>
-                    <span class="fw-light">ReactJS</span>
->>>>>>> main
+                    <span class="fw-light p" id="skills"></span>
+                    <script>
+                        document.getElementById("skills").innerHTML = skills_string;
+                    </script>
                 </div>
             </div>
             <div class="contacts row">
                 <div class="col">
-<<<<<<< HEAD
                     <span class="fw-bold h5">Email</span><br>
-                    <span class="fw-light p">whatever</span>
+                    <span class="fw-light p"><?php echo "$email"?></span>
                 </div>
                 <div class="col">
                     <span class="fw-bold h5">Phonenumber</span><br>
-                    <span class="fw-light p">whatever</span>
-=======
-                    <span class="fw-bold">Email</span><br>
-                    <span class="fw-light">whatever</span>
-                </div>
-                <div class="col">
-                    <span class="fw-bold">Phonenumber</span><br>
-                    <span class="fw-light">whatever</span>
->>>>>>> main
+                    <span class="fw-light p"><?php echo "$phone"?></span>
                 </div>
             </div>
         </div>
@@ -105,66 +84,91 @@
         <div class="col relative">
             <div class="row category experience">
                 <h2>Experience</h2>
-                <div class="category-item">
-                    <div class="brief mt-6">
-<<<<<<< HEAD
-                        <div class="title-2 fs-5 fw-bold"><?php echo "$job_title"?></div>
-=======
-                        <div class="title-2 fs-5 fw-bold"><?php echo "Creative director";?></div>
->>>>>>> main
-                        <span class="fw-light">(2018 - 2020)</span>
-                    </div>
-                    <span class="fw-bold fst-italic mb-6">Company name/Company location</span>
-                    <p class="fw-light mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sapiente saepe possimus esse culpa, inventore optio ipsa officiis iste, tempora aspernatur asperiores quaerat, nulla dolores dignissimos consequatur temporibus illo laborum?</p>
-                </div>
-                <div class="category-item">
-                    <div class="brief mt-6">
-                        <div class="title-2 fs-5 fw-bold">Creative director</div>
-                        <span class="fw-light">(2018 - 2020)</span>
-                    </div>
-                    <span class="fw-bold fst-italic mb-6">Company name/Company location</span>
-                    <p class="fw-light mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sapiente saepe possimus esse culpa, inventore optio ipsa officiis iste, tempora aspernatur asperiores quaerat, nulla dolores dignissimos consequatur temporibus illo laborum?</p>
-                </div>
+                <?php 
+                    $sql = "SELECT * FROM experiences WHERE cv_id = $id";
+                    $result = mysqli_query($conn, $sql);
+                    
+                    $num_row = mysqli_num_rows($result);
+                    // echo "$num_row";
+
+                    while ($rows = mysqli_fetch_assoc($result)){
+                        $job_title = $rows['job_title'];
+                        $start_date = $rows['start_date'];
+                        $end_date = $rows['end_date'];
+                        $comp_name = $rows['company'];
+                        $comp_loc = $rows['company_location'];
+                        $desp = $rows['description'];
+                        echo "
+                        <div class='category-item'>
+                            <div class='brief mt-6'>
+                                <div class='title-2 fs-5 fw-bold'>$job_title</div>
+                                <span class='fw-light'><span>(Start: $start_date/</span><span>End: $end_date)</span></span>
+                            </div>
+                            <span class='fw-bold fst-italic mb-6'>$comp_name/$comp_loc</span>
+                            <p class='fw-light mb-6'>$desp</p>
+                        </div>
+                        ";
+                    }
+                ?>
             </div>
             <div class="divider_line"></div>
             <div class="row category education">
                 <h2>Education</h2>
-                <div class="category-item">
-                    <div class="brief mt-6">
-                        <div class="title-2 fs-5 fw-bold">Creative director</div>
-                        <span class="fw-light">(2018 - 2020)</span>
-                    </div>
-                    <span class="fw-bold fst-italic mb-6">Company name/Company location</span>
-                    <p class="fw-light mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sapiente saepe possimus esse culpa, inventore optio ipsa officiis iste, tempora aspernatur asperiores quaerat, nulla dolores dignissimos consequatur temporibus illo laborum?</p>
-                </div>
-                <div class="category-item">
-                    <div class="brief mt-6">
-                        <div class="title-2 fs-5 fw-bold">Creative director</div>
-                        <span class="fw-light">(2018 - 2020)</span>
-                    </div>
-                    <span class="fw-bold fst-italic mb-6">Company name/Company location</span>
-                    <p class="fw-light mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sapiente saepe possimus esse culpa, inventore optio ipsa officiis iste, tempora aspernatur asperiores quaerat, nulla dolores dignissimos consequatur temporibus illo laborum?</p>
-                </div>
+                <?php 
+                    $sql = "SELECT * FROM education WHERE cv_id = $id";
+                    $result = mysqli_query($conn, $sql);
+                    
+                    $num_row = mysqli_num_rows($result);
+                    // echo "$num_row";
+
+                    while ($rows = mysqli_fetch_assoc($result)){
+                        $department = $rows['department'];
+                        $start_date = $rows['start_date'];
+                        $end_date = $rows['end_date'];
+                        $faculty = $rows['faculty'];
+                        $uni_loc = $rows['location'];
+                        $desp = $rows['description'];
+                        echo "
+                        <div class='category-item'>
+                            <div class='brief mt-6'>
+                                <div class='title-2 fs-5 fw-bold'>$department</div>
+                                <span class='fw-light'><span>(Start: $start_date/</span><span>End: $end_date)</span></span>
+                            </div>
+                            <span class='fw-bold fst-italic mb-6'>$faculty/$uni_loc</span>
+                            <p class='fw-light mb-6'>$desp</p>
+                        </div>
+                        ";
+                    }
+                ?>
             </div>
             <div class="divider_line"></div>
             <div class="row category certificate">
                 <h2>Certificate</h2>
-                <div class="category-item">
-                    <div class="brief mt-6">
-                        <div class="title-2 fs-5 fw-bold">Creative director</div>
-                        <span class="fw-light">(2018 - 2020)</span>
-                    </div>
-                    <span class="fw-bold fst-italic mb-6">Company name/Company location</span>
-                    <p class="fw-light mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sapiente saepe possimus esse culpa, inventore optio ipsa officiis iste, tempora aspernatur asperiores quaerat, nulla dolores dignissimos consequatur temporibus illo laborum?</p>
-                </div>
-                <div class="category-item">
-                    <div class="brief mt-6">
-                        <div class="title-2 fs-5 fw-bold">Creative director</div>
-                        <span class="fw-light">(2018 - 2020)</span>
-                    </div>
-                    <span class="fw-bold fst-italic mb-6">Company name/Company location</span>
-                    <p class="fw-light mb-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sapiente saepe possimus esse culpa, inventore optio ipsa officiis iste, tempora aspernatur asperiores quaerat, nulla dolores dignissimos consequatur temporibus illo laborum?</p>
-                </div>
+                <?php 
+                    $sql = "SELECT * FROM certificates WHERE cv_id = $id";
+                    $result = mysqli_query($conn, $sql);
+                    
+                    $num_row = mysqli_num_rows($result);
+                    // echo "$num_row";
+
+                    while ($rows = mysqli_fetch_assoc($result)){
+                        $certificate_name = $rows['certificate_name'];
+                        $start_date = $rows['start_date'];
+                        $end_date = $rows['end_date'];
+                        $issue_organization = $rows['issue_organization'];
+                        $desp = $rows['description'];
+                        echo "
+                        <div class='category-item'>
+                            <div class='brief mt-6'>
+                                <div class='title-2 fs-5 fw-bold'>$certificate_name</div>
+                                <span class='fw-light'><span>(Start: $start_date/</span><span>End: $end_date)</span></span>
+                            </div>
+                            <span class='fw-bold fst-italic mb-6'>$issue_organization</span>
+                            <p class='fw-light mb-6'>$desp</p>
+                        </div>
+                        ";
+                    }
+                ?>
             </div>
         </div>
     </div>
